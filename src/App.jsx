8c79778a89986ebcd8e4395582a98a40148dac90
Harold Cuico <64396407+ha-rold1999@users.noreply.github.com>
2023-06-01 19:41:49 +0800

    Revert "search and add note UI feature"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Card from "./Components/Card";
-import Header from "./Components/Header";
 
 function App() {
   let myarr = [
@@ -38,8 +37,13 @@ function App() {
   ];
 
   return (
-    <div className="w-full">
-      <Header />
+    <div className="w-full" style={{ backgroundColor: "#526D82" }}>
+      <div
+        className="w-full p-5  rounded-b-lg"
+        style={{ backgroundColor: "#9DB2BF" }}
+      >
+        <h1 className="font-bold text-3xl">My Notes</h1>
+      </div>
       <div className="flex flex-wrap justify-center w-full ">
         <div className="grid grid-cols-4">
           {myarr.map((arr, index) => {
