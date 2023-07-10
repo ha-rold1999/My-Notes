c2f825f92d27730de40af4e0ba113a216ee8a97b
ha-rold1999 <htarimanc@gmail.com>
2023-07-10 19:29:37 +0800

    Created a single component for the nav bar

--- src/Components/CodesAndSuch/CodesAndSuch.jsx
+++ src/Components/CodesAndSuch/CodesAndSuch.jsx
@@ -1,25 +1,35 @@
-import Header from "./Header"
-import Codes from "./Codes"
-import Code from "./Code"
-import { Routes, Route } from "react-router-dom"
-import Add from "./Add"
+import Codes from "./Codes";
+import Code from "./Code";
+import { Routes, Route } from "react-router-dom";
+import Add from "./Add";
+import Navbar from "../Utility/Navbar";
+import coding from "./../../assets/icon/coding.png";
+import newCode from "./../../assets/icon/branch.png";
 
 export default function CodesAndSuch() {
-  
+  const header = {
+    home: "/codes-and-such",
+    pageName: "Codes and Such",
+    logo: coding,
+    add: "/codes-and-such/add",
+    addName: "Add new Code",
+    addLogo: newCode,
+  };
+
   return (
     <div className="w-full">
       <div className="sticky top-0">
-        <Header />
+        <Navbar header={header} />
       </div>
       <div className="flex flex-wrap justify-center w-full ">
         <Routes>
-          <Route path="/" element={<Codes/>}/>
+          <Route path="/" element={<Codes />} />
           {/* <Route path="/add" element={<Add />} />
           <Route path="/note" element={<Note />} /> */}
-          <Route path="/code" element={<Code/>}/>
-          <Route path="/add" element={<Add/>}/>
+          <Route path="/code" element={<Code />} />
+          <Route path="/add" element={<Add />} />
         </Routes>
       </div>
     </div>
-  )
+  );
 }
--- src/Components/MyNotes/MyNotes.jsx
+++ src/Components/MyNotes/MyNotes.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import Notes from './Notes';
-import Header from './Header';
-import Add from "./Add"
-import Note from "./Note"
+import React from "react";
+import Notes from "./Notes";
+import Add from "./Add";
+import Note from "./Note";
+import Navbar from "../Utility/Navbar";
+import logo from "./../../assets/icon/note.png";
+import addIcon from "./../../assets/icon/add.png";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function MyNotes() {
+  const header = {
+    home: "/my-notes",
+    pageName: "My Notes",
+    logo: logo,
+    add: "/my-notes/add",
+    addName: "Add new Note",
+    addLogo: addIcon,
+  };
   return (
     <div className="w-full">
       <div className="sticky top-0">
-        <Header />
+        <Navbar header={header} />
       </div>
       <div className="flex flex-wrap justify-center w-full ">
         <Routes>
-          <Route path="/" element={<Notes/>}/>
+          <Route path="/" element={<Notes />} />
           <Route path="/add" element={<Add />} />
           <Route path="/note" element={<Note />} />
         </Routes>
       </div>
     </div>
-  )
+  );
 }
